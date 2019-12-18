9f71f831a6a4679e080e9a7a60e78f60bae252cd
ram071985 <reidmuchow@gmail.com>
2019-12-18 17:39:26 -0600

    added one key, 2 key and plus equals sign functionality.

--- calculator.js
+++ calculator.js
@@ -5,7 +5,37 @@ let screen = document.getElementById('js-screen');
 let one = document.getElementById('js-one');
 let two = document.getElementById('js-two');
 let plus = document.getElementById('js-plus');
-let screenText = screen.innerHTML = '1';
+let equal = document.getElementById('js-equals')
+
+
+// One Key Functionality
+one.addEventListener('click', function(e) {
+  let oneNode = document.createTextNode('1');
+  screen.appendChild(oneNode);
+  
+
+});
+
+// One Key Functionality
+two.addEventListener('click', function(e) {
+  let twoNode = document.createTextNode('2');
+  screen.appendChild(twoNode);
+  
+
+});
+
+// Plus key functionality
+plus.addEventListener('click', function(e) {
+  screen.innerHTML;
+  
+});
+
+// Equals sign Functionality
+equal.addEventListener('click', function() {
+  if ()
+
+});
+
 
 // let keys = ['one',
  //           'two',
@@ -14,21 +44,6 @@ let screenText = screen.innerHTML = '1';
 // keys[0] = document.getElementsByClassName('one')
 // keys[1] = document.getElementsByClassName('two')
 // keys[2] = document.getElementsByClassName('plus') 
-// keys[3] = document.getElementsByClassName('equals')                 
-
-
-// One Key Functionality
-one.addEventListener('click', function(e) {
-  let newText = document.createTextNode('1');
-  screen.appendChild(newText);
-  
-
-});
-
-plus.addEventListener('click', function(e) {
-  let elContent = screen;
-  console.log(elContent);
-});
-
+// keys[3] = document.getElementsByClassName('equals')               
 
 
